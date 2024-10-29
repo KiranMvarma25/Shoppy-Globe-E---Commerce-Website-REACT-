56cba74a5b81ab1161579575c251efef8fd04516
Kiran Varma <mkiranvarma123@gmail.com>
2024-10-30 01:00:16 +0530

    Added back to home and Written Project Details in README file

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,4 +1,7 @@
 import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 function NotFound(){
 
@@ -7,6 +10,10 @@ function NotFound(){
 
   return (
     <>
+      <div className="backToHomeParent">
+        <Link to="/" className="backToHome"><IoMdArrowRoundBack /> Home</Link>
+      </div>
+      
       <div className="notFoundParent">
         <h2 className="notFound">{err.status} {err.statusText}</h2>
         <h3 className="notFoundTwo">{err.data}</h3>
